53cc40a1daab7e45e4668688a61d883c27e0b770
Julio Decima <juliodecima@gmail.com>
2026-03-18 01:49:22 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5733239027-4f570",
+  "appId": "1:555482287833:web:404d2512c008ea291178c8",
+  "apiKey": "AIzaSyCFz3uaXfz9KHvtNq1fMTI_R8cD-IUm-V0",
+  "authDomain": "studio-5733239027-4f570.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "555482287833"
+};
