77699cc31b5dd6f47078b4f03cce0594bdd46750
Julio Decima <juliodecima@gmail.com>
2026-03-18 03:42:42 +0000

    Try fixing this error: `Runtime ReferenceError: cn is not defined. Error

--- src/app/admin/panels/page.tsx
+++ src/app/admin/panels/page.tsx
@@ -1,4 +1,3 @@
-
 "use client"
 
 import { useState, useMemo } from "react";
@@ -28,6 +27,7 @@ import { useCollection, useFirestore, useMemoFirebase } from "@/firebase";
 import { collection, query, doc, deleteDoc, updateDoc, orderBy } from "firebase/firestore";
 import { Panel } from "@/lib/types";
 import { useToast } from "@/hooks/use-toast";
+import { cn } from "@/lib/utils";
 
 export default function AdminPanelsPage() {
   const db = useFirestore();
